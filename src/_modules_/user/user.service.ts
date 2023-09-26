879f3af62d3ddc92f07a785fbb657d1c29a27cff
andrew <anntp@twendeesoft.com>
2023-09-26 16:14:29 +0700

    Fix schema and remove try catch

--- src/_modules_/user/user.service.ts
+++ src/_modules_/user/user.service.ts
@@ -9,43 +9,28 @@ export class UserService {
   constructor(private readonly prismaService: PrismaService) {}
 
   async create(createUserDto: CreateUserDto): Promise<User> {
-    try {
-      const user = await this.prismaService.user.create({
-        data: createUserDto,
-      });
-      return user;
-    } catch (error) {
-      console.log(error);
-      throw new Error(error);
-    }
+    const user = await this.prismaService.user.create({
+      data: createUserDto,
+    });
+    return user;
   }
 
   async findByStravaId(stravaId: number): Promise<User> {
-    try {
-      const findUser = await this.prismaService.user.findUnique({
-        where: { stravaId },
-      });
-      return findUser;
-    } catch (error) {
-      console.log(error);
-      throw new Error(error);
-    }
+    const findUser = await this.prismaService.user.findUnique({
+      where: { stravaId },
+    });
+    return findUser;
   }
 
   async changeToken(
     stravaId: number,
     changeTokenDto: ChangeTokenDto,
   ): Promise<User> {
-    try {
-      const { accessToken, accessTokenExpireTime, refreshToken } =
-        changeTokenDto;
-      const user = await this.prismaService.user.update({
-        where: { stravaId },
-        data: { accessToken, accessTokenExpireTime, refreshToken },
-      });
-      return user;
-    } catch (error) {
-      console.log(error);
-    }
+    const { accessToken, accessTokenExpireTime, refreshToken } = changeTokenDto;
+    const user = await this.prismaService.user.update({
+      where: { stravaId },
+      data: { accessToken, accessTokenExpireTime, refreshToken },
+    });
+    return user;
   }
 }
