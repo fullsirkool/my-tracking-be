fb73e4140a977b88db3d5e991b561f6d3a978229
andrew <adamaanvaan@gmail.com>
2023-12-04 10:33:25 +0700

    Complete User Endpoint

--- src/_modules_/auth/auth.controller.ts
+++ src/_modules_/auth/auth.controller.ts
@@ -1,56 +1,68 @@
 import {
-  Controller,
-  Param,
-  Post,
-  UseGuards,
-  Get,
-  UseInterceptors,
-  Body,
+    Controller,
+    Param,
+    Post,
+    UseGuards,
+    Get,
+    UseInterceptors,
+    Body, Patch,
 } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { RenewDto, SignInAdminDto } from './auth.dto';
-import { LocalAuthGuard } from 'src/guards/local-auth.guard';
-import { Claims, UserClaims } from 'src/types/auth.types';
-import { ApiBody, ApiTags } from '@nestjs/swagger';
-import { Admin } from 'src/decorators/admin.decorator';
-import { JwtAuthGuard } from 'src/guards/jwt-auth.guard';
-import { User } from 'src/decorators/user.decorator';
-import { AuthTransformInterceptor } from 'src/interceptors/auth.transform';
-import { JwtRefreshAuthGuard } from 'src/guards/jwt-refresh.guard';
+import {AuthService} from './auth.service';
+import {CompleteUserDto, RenewDto, SignInAdminDto} from './auth.dto';
+import {LocalAuthGuard} from 'src/guards/local-auth.guard';
+import {Claims, UserClaims} from 'src/types/auth.types';
+import {ApiBody, ApiTags} from '@nestjs/swagger';
+import {Admin} from 'src/decorators/admin.decorator';
+import {JwtAuthGuard} from 'src/guards/jwt-auth.guard';
+import {User} from 'src/decorators/user.decorator';
+import {AuthTransformInterceptor} from 'src/interceptors/auth.transform';
+import {JwtRefreshAuthGuard} from 'src/guards/jwt-refresh.guard';
+import {Auth} from "../../decorators/auth.decorator";
 
 @Controller('auth')
 @ApiTags('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+    constructor(private readonly authService: AuthService) {
+    }
 
-  @Post('/signin/:code')
-  @UseInterceptors(AuthTransformInterceptor)
-  async signin(@Param('code') code: string) {
-    const signInResponse = await this.authService.signIn(code);
-    return signInResponse;
-  }
+    @Post('/signin/:code')
+    @UseInterceptors(AuthTransformInterceptor)
+    async signin(@Param('code') code: string) {
+        const signInResponse = await this.authService.signIn(code);
+        return signInResponse;
+    }
 
-  @UseGuards(LocalAuthGuard)
-  @ApiBody({ type: SignInAdminDto })
-  @Post('/admin/sign-in')
-  async login(@Admin() claims: Claims) {
-    return this.authService.signInAdmin(claims);
-  }
+    @UseGuards(LocalAuthGuard)
+    @ApiBody({type: SignInAdminDto})
+    @Post('/admin/sign-in')
+    async login(@Admin() claims: Claims) {
+        return this.authService.signInAdmin(claims);
+    }
 
-  @UseGuards(JwtAuthGuard)
-  @Get('/self')
-  async getSelfInfo(@User() claims: UserClaims) {
-    console.log('claims', claims);
-    return this.authService.getSelfInfo(claims);
-  }
+    @UseGuards(JwtAuthGuard)
+    @Get('/self')
+    async getSelfInfo(@User() claims: UserClaims) {
+        console.log('claims', claims);
+        return this.authService.getSelfInfo(claims);
+    }
 
-  @Post('/renew')
-  @ApiBody({ type: RenewDto })
-  @UseGuards(JwtRefreshAuthGuard)
-  async refreshToken(
-    @User('id') userId: number,
-    @Body('refreshToken') refreshToken: string,
-  ) {
-    return this.authService.renewToken(userId, refreshToken);
-  }
+    @Post('/renew')
+    @ApiBody({type: RenewDto})
+    @UseGuards(JwtRefreshAuthGuard)
+    async refreshToken(
+        @User('id') userId: number,
+        @Body('refreshToken') refreshToken: string,
+    ) {
+        return this.authService.renewToken(userId, refreshToken);
+    }
+
+    @Patch('/complete')
+    @ApiBody({type: CompleteUserDto})
+    @Auth()
+    async complete(
+        @User('id') userId: number,
+        @Body() completeUserDto: CompleteUserDto,
+    ) {
+        return this.authService.complete(userId, completeUserDto);
+    }
 }
--- src/_modules_/auth/auth.service.ts
+++ src/_modules_/auth/auth.service.ts
@@ -1,250 +1,280 @@
-import { catchError, firstValueFrom } from 'rxjs';
-import { UserService } from './../user/user.service';
-import { PrismaService } from './../prisma/prisma.service';
+import {catchError, firstValueFrom} from 'rxjs';
+import {UserService} from './../user/user.service';
+import {PrismaService} from './../prisma/prisma.service';
 import {
-  Inject,
-  Injectable,
-  UnauthorizedException,
-  forwardRef,
+    Inject,
+    Injectable,
+    UnauthorizedException,
+    forwardRef, NotFoundException, ConflictException,
 } from '@nestjs/common';
-import { AuthDto, SignInAdminDto } from './auth.dto';
-import { HttpService } from '@nestjs/axios';
-import { AxiosError } from 'axios';
-import { AdminService } from '../admin/admin.service';
+import {AuthDto, CompleteUserDto, SignInAdminDto} from './auth.dto';
+import {HttpService} from '@nestjs/axios';
+import {AxiosError} from 'axios';
+import {AdminService} from '../admin/admin.service';
 import * as bcrypt from 'bcrypt';
 import {Claims, UserClaims} from 'src/types/auth.types';
-import { JwtService } from '@nestjs/jwt';
-import { exclude } from 'src/utils/transform.utils';
-import { ActivityService } from '../activity/activity.service';
-import { User } from '@prisma/client';
+import {JwtService} from '@nestjs/jwt';
+import {exclude} from 'src/utils/transform.utils';
+import {ActivityService} from '../activity/activity.service';
+import {User} from '@prisma/client';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private readonly prisma: PrismaService,
-    private readonly userService: UserService,
-    private readonly httpService: HttpService,
-    private readonly adminService: AdminService,
-    @Inject(forwardRef(() => ActivityService))
-    private readonly activityService: ActivityService,
-    private readonly jwtService: JwtService,
-  ) {}
+    constructor(
+        private readonly prisma: PrismaService,
+        private readonly userService: UserService,
+        private readonly httpService: HttpService,
+        private readonly adminService: AdminService,
+        @Inject(forwardRef(() => ActivityService))
+        private readonly activityService: ActivityService,
+        private readonly jwtService: JwtService,
+    ) {
+    }
 
-  async signIn(code: string): Promise<AuthDto> {
-    const url = `${process.env.STRAVA_BASE_URL}/oauth/token`;
-    const { data } = await firstValueFrom(
-      this.httpService
-        .post(
-          url,
-          {},
-          {
-            params: {
-              client_id: process.env.STRAVA_CLIENT_ID,
-              client_secret: process.env.STRAVA_CLIENT_SECRET,
-              code: code,
-              grant_type: `authorization_code`,
+    async signIn(code: string): Promise<AuthDto> {
+        const url = `${process.env.STRAVA_BASE_URL}/oauth/token`;
+        const {data} = await firstValueFrom(
+            this.httpService
+                .post(
+                    url,
+                    {},
+                    {
+                        params: {
+                            client_id: process.env.STRAVA_CLIENT_ID,
+                            client_secret: process.env.STRAVA_CLIENT_SECRET,
+                            code: code,
+                            grant_type: `authorization_code`,
+                        },
+                    },
+                )
+                .pipe(
+                    catchError((error: AxiosError) => {
+                        console.error(error.response.data);
+                        throw 'An error happened!';
+                    }),
+                ),
+        );
+        const {refresh_token, access_token} = data;
+        const {
+            id,
+            firstname,
+            lastname,
+            bio,
+            city,
+            state,
+            country,
+            sex,
+            profile_medium,
+            profile,
+        } = data.athlete;
+        const sendUser = {
+            stravaId: id,
+            firstName: firstname,
+            lastName: lastname,
+            bio,
+            city,
+            state,
+            country,
+            sex,
+            profileMedium: profile_medium,
+            profile,
+            stravaRefreshToken: refresh_token,
+        };
+        const findUser = await this.userService.findByStravaId(id);
+        if (findUser) {
+            const changeTokenDto = {
+                stravaRefreshToken: refresh_token,
+            };
+            const user = await this.userService.changeToken(id, changeTokenDto);
+            const {accessToken, refreshToken} = await this.generateTokens(user);
+            return {
+                user,
+                accessToken,
+                refreshToken,
+                expireTime: +process.env.JWT_ACCESS_TOKEN_EXPIRATION_TIME,
+            };
+        }
+
+        const user = await this.userService.create(sendUser);
+        await this.activityService.createMany({
+            user,
+            access_token,
+        });
+        const {accessToken, refreshToken} = await this.generateTokens(user);
+        return {
+            user,
+            accessToken,
+            refreshToken,
+            expireTime: +process.env.JWT_ACCESS_TOKEN_EXPIRATION_TIME,
+        };
+    }
+
+    async resetToken(stravaRefreshToken: string) {
+        const url = `${process.env.STRAVA_BASE_URL}/oauth/token`;
+        const {data} = await firstValueFrom(
+            this.httpService
+                .post(
+                    url,
+                    {},
+                    {
+                        params: {
+                            client_id: process.env.STRAVA_CLIENT_ID,
+                            client_secret: process.env.STRAVA_CLIENT_SECRET,
+                            grant_type: `refresh_token`,
+                            refresh_token: stravaRefreshToken,
+                        },
+                    },
+                )
+                .pipe(
+                    catchError((error: AxiosError) => {
+                        console.error(error);
+                        throw 'An error happened!';
+                    }),
+                ),
+        );
+
+        return data;
+    }
+
+    async validateAdmin(signInAdminDto: SignInAdminDto) {
+        const {username, password} = signInAdminDto;
+        const admin = await this.adminService.findByUsername(username);
+        if (!admin) {
+            throw new UnauthorizedException('Admin not found!');
+        }
+
+        const isMatchPassword = await bcrypt.compare(password, admin.password);
+
+        if (!isMatchPassword) {
+            throw new UnauthorizedException('Wrong email or password!');
+        }
+
+        return admin;
+    }
+
+    async signInAdmin(claims: Claims) {
+        const [tokens, admin] = await Promise.all([
+            this.generateAdminTokens(claims),
+            this.prisma.admin.findUnique({
+                where: {id: claims.id},
+            }),
+        ]);
+        return {
+            ...tokens,
+            admin: exclude(admin, ['password', 'refreshToken']),
+        };
+    }
+
+    async renewToken(userId: number, refreshToken: string) {
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: userId,
             },
-          },
-        )
-        .pipe(
-          catchError((error: AxiosError) => {
-            console.error(error.response.data);
-            throw 'An error happened!';
-          }),
-        ),
-    );
-    const { refresh_token, access_token } = data;
-    const {
-      id,
-      firstname,
-      lastname,
-      bio,
-      city,
-      state,
-      country,
-      sex,
-      profile_medium,
-      profile,
-    } = data.athlete;
-    const sendUser = {
-      stravaId: id,
-      firstName: firstname,
-      lastName: lastname,
-      bio,
-      city,
-      state,
-      country,
-      sex,
-      profileMedium: profile_medium,
-      profile,
-      stravaRefreshToken: refresh_token,
-    };
-    const findUser = await this.userService.findByStravaId(id);
-    if (findUser) {
-      const changeTokenDto = {
-        stravaRefreshToken: refresh_token,
-      };
-      const user = await this.userService.changeToken(id, changeTokenDto);
-      const { accessToken, refreshToken } = await this.generateTokens(user);
-      return {
-        user,
-        accessToken,
-        refreshToken,
-        expireTime: +process.env.JWT_ACCESS_TOKEN_EXPIRATION_TIME,
-      };
+        });
+
+        if (refreshToken !== user.refreshToken) {
+            throw new UnauthorizedException();
+        }
+
+        const tokens = await this.generateTokens(user);
+        return tokens;
     }
 
-    const user = await this.userService.create(sendUser);
-    await this.activityService.createMany({
-      user,
-      access_token,
-    });
-    const { accessToken, refreshToken } = await this.generateTokens(user);
-    return {
-      user,
-      accessToken,
-      refreshToken,
-      expireTime: +process.env.JWT_ACCESS_TOKEN_EXPIRATION_TIME,
-    };
-  }
+    private async generateAdminTokens(claims: Claims) {
+        const accessToken = this.jwtService.sign(claims, {
+            expiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRATION_TIME,
+            secret: process.env.ACCESS_TOKEN_SECRET,
+        });
 
-  async resetToken(stravaRefreshToken: string) {
-    const url = `${process.env.STRAVA_BASE_URL}/oauth/token`;
-    const { data } = await firstValueFrom(
-      this.httpService
-        .post(
-          url,
-          {},
-          {
-            params: {
-              client_id: process.env.STRAVA_CLIENT_ID,
-              client_secret: process.env.STRAVA_CLIENT_SECRET,
-              grant_type: `refresh_token`,
-              refresh_token: stravaRefreshToken,
+        const refreshToken = this.jwtService.sign(
+            {sub: claims.id},
+            {
+                expiresIn: process.env.JWT_REFRESH_TOKEN_EXPIRATION_TIME,
+                secret: process.env.REFRESH_TOKEN_SECRET,
             },
-          },
-        )
-        .pipe(
-          catchError((error: AxiosError) => {
-            console.error(error);
-            throw 'An error happened!';
-          }),
-        ),
-    );
+        );
 
-    return data;
-  }
+        await this.prisma.admin.update({
+            where: {id: claims.id},
+            data: {
+                refreshToken,
+            },
+        });
 
-  async validateAdmin(signInAdminDto: SignInAdminDto) {
-    const { username, password } = signInAdminDto;
-    const admin = await this.adminService.findByUsername(username);
-    if (!admin) {
-      throw new UnauthorizedException('Admin not found!');
+        return {
+            accessToken,
+            refreshToken,
+        };
     }
 
-    const isMatchPassword = await bcrypt.compare(password, admin.password);
+    private async generateTokens(user: User) {
+        const {id, stravaId, firstName, lastName, profile, refreshToken} = user;
+        const accessToken = this.jwtService.sign(
+            {id, stravaId, firstName, lastName, profile},
+            {
+                expiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRATION_TIME,
+                secret: process.env.ACCESS_TOKEN_SECRET,
+            },
+        );
 
-    if (!isMatchPassword) {
-      throw new UnauthorizedException('Wrong email or password!');
+        if (!refreshToken) {
+            const newRefreshToken = this.jwtService.sign(
+                {sub: id},
+                {
+                    expiresIn: process.env.JWT_REFRESH_TOKEN_EXPIRATION_TIME,
+                    secret: process.env.REFRESH_TOKEN_SECRET,
+                },
+            );
+
+            await this.prisma.user.update({
+                where: {id: id},
+                data: {
+                    refreshToken: newRefreshToken,
+                },
+            });
+
+            return {
+                accessToken,
+                refreshToken: newRefreshToken,
+            };
+        }
+
+        return {
+            accessToken,
+            refreshToken,
+        };
     }
 
-    return admin;
-  }
-
-  async signInAdmin(claims: Claims) {
-    const [tokens, admin] = await Promise.all([
-      this.generateAdminTokens(claims),
-      this.prisma.admin.findUnique({
-        where: { id: claims.id },
-      }),
-    ]);
-    return {
-      ...tokens,
-      admin: exclude(admin, ['password', 'refreshToken']),
-    };
-  }
-
-  async renewToken(userId: number, refreshToken: string) {
-    const user = await this.prisma.user.findUnique({
-      where: {
-        id: userId,
-      },
-    });
-
-    if (refreshToken !== user.refreshToken) {
-      throw new UnauthorizedException();
+    getSelfInfo(claims: UserClaims) {
+        return {}
     }
 
-    const tokens = await this.generateTokens(user);
-    return tokens;
-  }
+    async complete(userId, completeUserDto: CompleteUserDto) {
+        const {email, password} = completeUserDto
 
-  private async generateAdminTokens(claims: Claims) {
-    const accessToken = this.jwtService.sign(claims, {
-      expiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRATION_TIME,
-      secret: process.env.ACCESS_TOKEN_SECRET,
-    });
+        const user = await this.prisma.user.findUnique({
+            where: {
+                id: userId
+            }
+        })
+        if (!user) {
+            throw new NotFoundException('Not Found User!')
+        }
 
-    const refreshToken = this.jwtService.sign(
-      { sub: claims.id },
-      {
-        expiresIn: process.env.JWT_REFRESH_TOKEN_EXPIRATION_TIME,
-        secret: process.env.REFRESH_TOKEN_SECRET,
-      },
-    );
+        if (user.email && user.password) {
+            throw new ConflictException('User has been completed!')
+        }
 
-    await this.prisma.admin.update({
-      where: { id: claims.id },
-      data: {
-        refreshToken,
-      },
-    });
+        const completeUser = await this.prisma.user.update({
+            data: {
+                email,
+                password
+            },
+            where: {
+                id: user.id
+            }
+        })
 
-    return {
-      accessToken,
-      refreshToken,
-    };
-  }
-
-  private async generateTokens(user: User) {
-    const { id, stravaId, firstName, lastName, profile, refreshToken } = user;
-    const accessToken = this.jwtService.sign(
-      { id, stravaId, firstName, lastName, profile },
-      {
-        expiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRATION_TIME,
-        secret: process.env.ACCESS_TOKEN_SECRET,
-      },
-    );
-
-    if (!refreshToken) {
-      const newRefreshToken = this.jwtService.sign(
-        { sub: id },
-        {
-          expiresIn: process.env.JWT_REFRESH_TOKEN_EXPIRATION_TIME,
-          secret: process.env.REFRESH_TOKEN_SECRET,
-        },
-      );
-
-      await this.prisma.user.update({
-        where: { id: id },
-        data: {
-          refreshToken: newRefreshToken,
-        },
-      });
-
-      return {
-        accessToken,
-        refreshToken: newRefreshToken,
-      };
+        return {success: true}
     }
-
-    return {
-      accessToken,
-      refreshToken,
-    };
-  }
-
-  getSelfInfo(claims: UserClaims) {
-    return {}
-  }
 }
