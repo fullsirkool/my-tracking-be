31e56ce8737d3dbcd858b6486a82635b211e72ee
andrew <adamaanvaan@gmail.com>
2024-03-01 17:35:00 +0700

    Fix config port

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  const port = +process.env.PORT || 5000;
+  const port = +process.env.PORT || 8080;
   await app.listen(port);
 }
 bootstrap();
