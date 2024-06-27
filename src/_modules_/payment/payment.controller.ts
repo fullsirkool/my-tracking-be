c025ef250119d4571802d3ad2ca0f7c2db9fcae8
Andrew Ngo <adamaanvaan@gmail.com>
2024-06-27 14:36:35 +0700

    Fix event header type

--- src/_modules_/payment/payment.controller.ts
+++ src/_modules_/payment/payment.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post, Sse } from '@nestjs/common';
+import {Body, Controller, Post, Res, Sse} from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { CompletePaymentDto } from './payment.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { fromEvent, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { Response } from 'express';
 
 @Controller('payment')
 @ApiTags('payment')
@@ -20,7 +21,11 @@ export class PaymentController {
   }
 
   @Sse('sse')
-  sse(): Observable<MessageEvent> {
+  sse(@Res() res: Response): Observable<MessageEvent> {
+    res.setHeader('Content-Type', 'text/event-stream');
+    res.setHeader('Cache-Control', 'no-cache');
+    res.setHeader('Connection', 'keep-alive');
+    res.flushHeaders();
     console.log('send event!');
     return fromEvent(this.eventEmitter, 'new-payment').pipe(
       map((data) => {
