9c9160c91cfb96c38e2dc56f31bc7692160e6194
Andrew Ngo <adamaanvaan@gmail.com>
2024-06-27 14:57:10 +0700

    Fix server send event

--- src/_modules_/payment/payment.controller.ts
+++ src/_modules_/payment/payment.controller.ts
@@ -1,11 +1,10 @@
-import { Body, Controller, Post, Res, Sse } from '@nestjs/common';
+import { Body, Controller, Post, Sse } from '@nestjs/common';
 import { PaymentService } from './payment.service';
 import { CompletePaymentDto } from './payment.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { EventEmitter2 } from '@nestjs/event-emitter';
 import { fromEvent, Observable, take } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { Response } from 'express';
 
 @Controller('payment')
 @ApiTags('payment')
@@ -21,14 +20,9 @@ export class PaymentController {
   }
 
   @Sse('sse')
-  sse(@Res() res: Response): Observable<MessageEvent> {
-    res.setHeader('Content-Type', 'text/event-stream');
-    res.setHeader('Cache-Control', 'no-cache');
-    res.setHeader('Connection', 'keep-alive');
-    res.flushHeaders();
+  sse(): Observable<MessageEvent> {
     console.log('send event!');
     return fromEvent(this.eventEmitter, 'new-payment').pipe(
-      take(1),
       map((data) => {
         return new MessageEvent('new-payment', { data: 'new payment' });
       }),
